ca575b4ddb8f78c1952d5eaa6f18f4d6c9f39fd6
Denis Volchenko <denisvolchenko2@gmail.com>
2024-03-02 22:28:56 +0300

    -Сделал тесты для создание пользователя

---
+++ src/main/java/constants/EndPoints.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class EndPoints {
+    public static final String URL = "https://stellarburgers.nomoreparties.site";
+//    https://code.s3.yandex.net/qa-automation-engineer/java/cheatsheets/paid-track/diplom/api-documentation.pdf
+    public static final String REGISTER = "/api/auth/register";//POST
+    public static final String LOGIN = "/api/auth/login";//POST
+    public  static final String GET_USER = "/api/auth/user";//GET
+
+}
---
+++ src/test/java/user/RegisterTests.java
@@ -0,0 +1,67 @@
+package user;
+
+import base.BaseHttpClient;
+import base.PostApi;
+import constants.EndPoints;
+import io.qameta.allure.Step;
+import json.RegisterRequsetCard;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class RegisterTests {
+    private PostApi postApi = new PostApi();
+    private RegisterRequsetCard registerCard;
+    @Before
+    public void setUp(){
+         registerCard = new RegisterRequsetCard(
+                "a"+ BaseHttpClient.getRandomIndex() +"@a.com",
+                "123",
+                "Sasha"
+        );
+    }
+    @Test
+    public void registerTest(){
+        postApi.doPost(EndPoints.REGISTER,registerCard)
+                .then().statusCode(200)
+                .and().assertThat().body("success",equalTo(true))
+                .and().assertThat().body("user.email",equalTo(registerCard.getEmail()));
+    }
+
+    @Test
+    public void duplicateRegisterTest(){
+        postApi.doPost(EndPoints.REGISTER,registerCard);
+        postApi.doPost(EndPoints.REGISTER,registerCard)
+                .then().statusCode(403)
+                .and().assertThat().body("message",equalTo("User already exists"));
+    }
+    @Test
+    public void noEmailRegisterTest(){
+        registerCard.setEmail("");
+        assertThatNoRequiredField();
+    }
+    @Test
+    public void noPasswordRegisterTest(){
+        registerCard.setPassword("");
+        assertThatNoRequiredField();
+    }
+    @Test
+    public void noNameRegisterTest(){
+        registerCard.setName("");
+        assertThatNoRequiredField();
+    }
+    @Step
+    public void assertThatNoRequiredField(){
+        postApi.doPost(EndPoints.REGISTER,registerCard)
+                .then().statusCode(403)
+                .and().assertThat().body("message",equalTo("Email, password and name are required fields"));
+    }
+
+    @After
+    public void cleanUp(){
+        //todo сделать удалеине созданного пользователя
+    }
+
+}
